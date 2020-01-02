a7e8aa34e1c361894fe928560b92f49849679f2b
TeamUndefinedFTC <55640178+TeamUndefinedFTC@users.noreply.github.com>
2020-01-01 20:33:19 -0500

    Add files via upload

--- DuoDou.java
+++ DuoDou.java
@@ -25,40 +25,27 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */package org.firstinspires.ftc.teamcode;
+ */
+package org.firstinspires.ftc.teamcode;
 
-import android.util.Log;
-
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.opencv.core.Mat;
-
-import detectors.FoundationPipeline.Foundation;
-import detectors.FoundationPipeline.Pipeline;
-import detectors.FoundationPipeline.SkyStone;
-import detectors.FoundationPipeline.Stone;
-import detectors.ImageDetector;
-
-import java.util.Iterator;
-import java.util.List;
-
-import detectors.ImageDetector;
-import detectors.OpenCvDetector;
-import detectors.StoneDetector;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.opencv.core.Mat;
+
+import java.util.Iterator;
+import java.util.List;
+
+import detectors.FoundationPipeline.Pipeline;
+import detectors.FoundationPipeline.SkyStone;
+import detectors.OpenCvDetector;
 
 @Autonomous(name = "The Three <<Holy Systems>>", group = "Primordial Artifact")
 public class DuoDou extends LinearOpMode {
@@ -79,9 +66,9 @@ public class DuoDou extends LinearOpMode {
     private Servo sweepRight;
 
     // imu stuff
-    BNO055IMU               imu;
-    Orientation             lastAngles = new Orientation();
-    double                  globalAngle, power = .30, correction;
+    BNO055IMU imu;
+    Orientation lastAngles = new Orientation();
+    double globalAngle, power = .30, correction;
 
     public void runOpMode() throws InterruptedException {
 
@@ -111,7 +98,7 @@ public class DuoDou extends LinearOpMode {
         ---------------------------------------------------------
          */
 
-		OpenCvDetector fieldDetector = new OpenCvDetector(this);
+        OpenCvDetector fieldDetector = new OpenCvDetector(this);
         //ImageDetector detector = new ImageDetector(this, false);
         //StoneDetector stone = new StoneDetector(this, false);
 
@@ -124,10 +111,10 @@ public class DuoDou extends LinearOpMode {
          */
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 
-        parameters.mode                = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled      = false;
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
 
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
         // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
@@ -140,8 +127,7 @@ public class DuoDou extends LinearOpMode {
         telemetry.addData("Mode", "calibrating...");
         telemetry.update();
 
-        while (!isStopRequested() && !imu.isGyroCalibrated())
-        {
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
             sleep(50);
             idle();
         }
@@ -152,91 +138,47 @@ public class DuoDou extends LinearOpMode {
         /*
         ----------------------------------------------------
          */
+
+        // block deetection
+
         while (!isStopRequested()) {
             //detector.printposition(detector.getPosition());
-
             //fieldDetector.print(fieldDetector.getObjectsFoundations());
             //Log.d("GO TO MO","go");
-
             SkyStone[] skyStone = fieldDetector.getObjectsSkyStones();
 
-            telemetry.addData("Skystones Found",skyStone.length);
-            int i = 0;
-            for(SkyStone s : skyStone){
-                i++;
-                telemetry.addLine(""+s);
+            telemetry.addData("Skystones Found", skyStone.length);
+            int p = 0;
+            for (SkyStone s : skyStone) {
+                p++;
+                telemetry.addLine("" + s);
             }
 
             Mat m = new Mat();
             m = null;
 
             //also, enabling/disabling detection for individual field elements:
-            /*
-            Pipeline.doFoundations=false;
-            Pipeline.doStones=false;
-            Pipeline.doSkyStones=false;
-            */
-
-
-            //getting positions of each element. Will return Objects for each element that all contain
-            // Phone camera <x,y> coordinates for each element's center.
-            /*
-            List<Foundation> foundations = Pipeline.foundations;
-            List<Stone> stones = Pipeline.stones;
-            List<SkyStone> skystones = Pipeline.skyStones;
-            */
-
-
-
-            //stone.print(stone.getObjectsFoundations());
-
-            //imu.printposition(imu.getDeltaPosition());
-
-            telemetry.addData("==========", "Loop delimiter");
-
-            telemetry.update();
-        }
-        // block deetection
-        while (opModeIsActive()) {
-            //detector.printposition(detector.getPosition());
-            //fieldDetector.print(fieldDetector.getObjectsFoundations());
-	        //Log.d("GO TO MO","go");
-            for(int i = 0; i < 1000; i++) {
-                SkyStone[] skyStone = fieldDetector.getObjectsSkyStones();
-
-                telemetry.addData("Skystones Found", skyStone.length);
-                int p = 0;
-                for (SkyStone s : skyStone) {
-                    p++;
-                    telemetry.addLine("" + s);
-                }
-
-                Mat m = new Mat();
-                m = null;
-
-                //also, enabling/disabling detection for individual field elements:
                 /*
                 Pipeline.doFoundations=false;
                 Pipeline.doStones=false;
                 Pipeline.doSkyStones=false;
                 */
+
+            // getting positions of each element. Will return Objects for each element that all contain
+            // Phone camera <x,y> coordinates for each element's center.
+
+            // List<Foundation> foundations = Pipeline.foundations;
+            //List<Stone> stones = Pipeline.stones;
+            List<SkyStone> skystones = Pipeline.skyStones;
+
+            for (int j = 0; j < skystones.size(); j++) {
+                telemetry.addData("Skystone center", "(" + skystones.get(j).x + ", " + skystones.get(j).y + ")");
             }
-
-                // getting positions of each element. Will return Objects for each element that all contain
-                // Phone camera <x,y> coordinates for each element's center.
-
-                List<Foundation> foundations = Pipeline.foundations;
-                List<Stone> stones = Pipeline.stones;
-                List<SkyStone> skystones = Pipeline.skyStones;
-
-                for(int j = 0; j < skystones.size(); j++) {
-                    telemetry.addData("Skystone center", "(" + skystones.get(j).x + ", " + skystones.get(j).y + ")");
-                }
 
                 // finding stone closesst to camera
                 SkyStone closestStone = null;
-                for(int j = 0; j < skystones.size(); j++) {
-                    if(skystones.size() > 1) {
+                if(skystones.size() > 1) {
+                    for(int j = 0; j < skystones.size(); j++) {
                         double closest;
                         closest = Math.abs(skystones.get(0).x - 300) + Math.abs(skystones.get(0).y - 300);
                         for(int k = 0; k < skystones.size(); k++) {
@@ -244,6 +186,9 @@ public class DuoDou extends LinearOpMode {
                                 closestStone = skystones.get(k);
                         }
                     }
+                }
+                else {
+                    closestStone = skystones.get(0);
                 }
 
                 telemetry.addData("==========", "Loop delimiter");
@@ -253,10 +198,10 @@ public class DuoDou extends LinearOpMode {
                     goBackward(0.1);
                 }
 
-                rotate(-90, 0.1);
-                collect(0.75);
-                stopMotors();
-                goBackward(0.75);
+            rotate(-90, 0.1);
+            collect(0.75);
+            stopMotors();
+            goBackward(0.75);
 
         }
 
@@ -276,8 +221,7 @@ public class DuoDou extends LinearOpMode {
     /*
      * Resets the cumulative angle tracking to zero.
      */
-    private void resetAngle()
-    {
+    private void resetAngle() {
         lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
         globalAngle = 0;
@@ -287,8 +231,7 @@ public class DuoDou extends LinearOpMode {
      * Get current cumulative angle rotation from last reset.
      * @return Angle in degrees. + = left, - = right.
      */
-    private double getAngle()
-    {
+    private double getAngle() {
         // We experimentally determined the Z axis is the axis we want to use for heading angle.
         // We have to process the angle because the imu works in euler angles so the Z axis is
         // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
@@ -312,10 +255,10 @@ public class DuoDou extends LinearOpMode {
 
     /**
      * See if we are moving in a straight line and if not return a power correction value.
+     *
      * @return Power adjustment, + is adjust left - is adjust right.
      */
-    private double checkDirection()
-    {
+    private double checkDirection() {
         // The gain value determines how sensitive the correction is to direction changes.
         // You will have to experiment with your robot to get small smooth direction changes
         // to stay on a straight line.
@@ -338,9 +281,8 @@ public class DuoDou extends LinearOpMode {
      * @param degrees Degrees to turn, + is left - is right
      * ------------------------------------------------------
      */
-    private void rotate(int degrees, double power)
-    {
-        double  leftPower, rightPower;
+    private void rotate(int degrees, double power) {
+        double leftPower, rightPower;
 
         // restart imu movement tracking.
         resetAngle();
@@ -348,32 +290,29 @@ public class DuoDou extends LinearOpMode {
         // getAngle() returns + when rotating counter clockwise (left) and - when rotating
         // clockwise (right).
 
-        if (degrees < 0)
-        {   // turn right.
+        if (degrees < 0) {   // turn right.
             leftPower = power;
             rightPower = -power;
-        }
-        else if (degrees > 0)
-        {   // turn left.
+        } else if (degrees > 0) {   // turn left.
             leftPower = -power;
             rightPower = power;
-        }
-        else return;
+        } else return;
 
         // set power to rotate.
         frontLeft.setPower(leftPower);
         frontRight.setPower(rightPower);
 
         // rotate until turn is completed.
-        if (degrees < 0)
-        {
+        if (degrees < 0) {
             // On right turn we have to get off zero first.
-            while (opModeIsActive() && getAngle() == 0) {}
+            while (opModeIsActive() && getAngle() == 0) {
+            }
 
-            while (opModeIsActive() && getAngle() > degrees) {}
-        }
-        else    // left turn.
-            while (opModeIsActive() && getAngle() < degrees) {}
+            while (opModeIsActive() && getAngle() > degrees) {
+            }
+        } else    // left turn.
+            while (opModeIsActive() && getAngle() < degrees) {
+            }
 
         // turn the motors off.
         frontLeft.setPower(leftPower);
@@ -400,6 +339,7 @@ public class DuoDou extends LinearOpMode {
         sweepLeft.setPosition(0);
         sweepRight.setPosition(1);
     }
+
     public void goForward(double tgtPower) {
         frontRight.setPower(-tgtPower);
         frontLeft.setPower(tgtPower);
@@ -463,6 +403,5 @@ public class DuoDou extends LinearOpMode {
         }
 
         telemetry.setAutoClear(true);
-
     }
 }
