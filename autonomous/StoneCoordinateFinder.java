87a26cce17c7b0268b66aa8287d5d136553ea576
TeamUndefinedFTC <55640178+TeamUndefinedFTC@users.noreply.github.com>
2020-01-02 20:21:01 -0500

    Update and rename StoneCoordinateFinder to autonomous/StoneCoordinateFinder.java

--- autonomous/StoneCoordinateFinder.java
+++ autonomous/StoneCoordinateFinder.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.detectors.DogeCVDetector;
