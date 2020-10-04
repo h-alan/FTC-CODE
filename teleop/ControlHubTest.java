7ab88a47cbdc7f761ead4af7c1b3b11befd6c4c7
TeamUndefinedFTC <55640178+TeamUndefinedFTC@users.noreply.github.com>
2020-10-04 17:54:06 -0400

    Update ControlHubTest.java

--- teleop/ControlHubTest.java
+++ teleop/ControlHubTest.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.teleop;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 /**
@@ -30,6 +33,10 @@ public class ControlHubTest extends LinearOpMode {
     private DcMotor backRight;
     private DcMotor backLeft;
 
+    //Distance Sensors
+    private Rev2mDistanceSensor top;
+    private Rev2mDistanceSensor bottom;
+
     double motorPower = 0.75;
 
     @Override
@@ -43,12 +50,14 @@ public class ControlHubTest extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
 
+        top = hardwareMap.get(Rev2mDistanceSensor.class, "top");
+        bottom = hardwareMap.get(Rev2mDistanceSensor.class, "bottom");
+
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
             if (gamepad1.left_stick_x < -0.85) {
                 strafeLeft(motorPower);
             } else if (gamepad1.left_stick_x > 0.85) {
@@ -59,8 +68,23 @@ public class ControlHubTest extends LinearOpMode {
                 backRight.setPower(motorPower * (-this.gamepad1.left_stick_y + this.gamepad1.left_stick_x - this.gamepad1.right_stick_x));
                 backLeft.setPower(motorPower * (this.gamepad1.left_stick_y + this.gamepad1.left_stick_x - this.gamepad1.right_stick_x));
             }
+            int rings;
+            if(bottom.getDistance(DistanceUnit.MM) < 200) {
+                if (top.getDistance(DistanceUnit.MM) < 200) {
+                    rings = 4;
+                }
+                else {
+                    rings = 1;
+                }
+            }
+            else{
+                rings = 0;
+            }
             telemetry.addData("Status", "Running");
             telemetry.addData("Motor Power: ", motorPower);
+            telemetry.addData("Top Distance:", top.getDistance(DistanceUnit.MM));
+            telemetry.addData("Bottom Distance:", bottom.getDistance(DistanceUnit.MM));
+            telemetry.addData("Rings:", rings);
             telemetry.update();
         }
     }
