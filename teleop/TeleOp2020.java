6acf09e0caddfd45b7659c5a2819e99ba85e872a
TeamUndefinedFTC <55640178+TeamUndefinedFTC@users.noreply.github.com>
2020-12-30 14:30:33 -0500

    Update TeleOp2020.java

--- teleop/TeleOp2020.java
+++ teleop/TeleOp2020.java
@@ -17,6 +17,7 @@ public class TeleOp2020 extends ThreadOpMode {
     --------------------------------------
      */
 
+    // moving motors
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
@@ -26,13 +27,15 @@ public class TeleOp2020 extends ThreadOpMode {
     private DcMotor wheelRack;
     private DcMotor belt;
 
+    // launching motor
     private DcMotor launcher;
 
     // servos
     private Servo wobbleArm;
-    private Servo launcherPush;
+    private Servo launcherPush;  // moves the rings into the launching motor
 
     double motorPower = 0.75;
+    double launcherPower = 0.75;
     /*
     --------------------------------------
     */
@@ -49,13 +52,12 @@ public class TeleOp2020 extends ThreadOpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
 
+        // Player 2
         launcher = hardwareMap.get(DcMotor.class, "launcher");
         launcherPush = hardwareMap.get(Servo.class, "launcherPush");
 
         wheelRack = hardwareMap.get(DcMotor.class, "wheelRack");
         belt = hardwareMap.get(DcMotor.class, "belt");
-
-        // Player 2
         //arm is god servo
         //wobbleArm = hardwareMap.get(Servo.class, "arm");
 
@@ -81,15 +83,16 @@ public class TeleOp2020 extends ThreadOpMode {
             }
         }));
 
+        // collecting rings through the belt
         registerThread(new TaskThread(new TaskThread.Actions() {
             @Override
             public void loop() {
                 if (gamepad2.left_stick_y > 0.2) {
-                    wheelRack.setPower(1);
-                    belt.setPower(0.5);
-                } else if (gamepad2.left_stick_y < -0.2) {
                     wheelRack.setPower(-1);
                     belt.setPower(-0.5);
+                } else if (gamepad2.left_stick_y < -0.2) {
+                    wheelRack.setPower(1);
+                    belt.setPower(0.5);
                 } else {
                     wheelRack.setPower(0);
                     belt.setPower(0);
@@ -97,18 +100,23 @@ public class TeleOp2020 extends ThreadOpMode {
             }
         }));
 
-        // launcher controls
+        /*
+         *   launcher controls
+         */
+
+        // motor
         registerThread(new TaskThread(new TaskThread.Actions() {
             @Override
             public void loop() {
                 if (gamepad2.left_trigger > 0.85) {
-                    launcher.setPower(0.72);
+                    launcher.setPower(launcherPower);
                 } else {
                     launcher.setPower(0);
                 }
             }
         }));
 
+        // servo
         registerThread(new TaskThread(new TaskThread.Actions() {
             @Override
             public void loop() {
@@ -119,9 +127,104 @@ public class TeleOp2020 extends ThreadOpMode {
                 }
             }
         }));
+        /*
+         */
+
+        /*
+         *    motorPower
+         */
+
+        // increasing
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            boolean increase = true; //Outside of loop()
+
+            @Override
+            public void loop() {
+                if (gamepad1.right_bumper && increase) {
+                    motorPower += 0.25;
+                    if (motorPower > 1) {
+                        motorPower = 1;
+                    }
+                    increase = false; // won't increase the motor power if the button is held down
+                } else if (!gamepad1.right_bumper)
+                    increase = true; // releasing the bumper  will allow you to increase again
+
+                motorPower = Math.round(motorPower * 100.0) / 100.0; // motor power is sometimes wacky and will have infinite decimals, rounding to fix that
+            }
+        }));
+
+        // decreasing
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            boolean decrease = true; //Outside of loop()
+
+            @Override
+            public void loop() {
+                if (gamepad1.left_bumper && decrease) {
+                    motorPower -= 0.25;
+                    if (motorPower < 0) {
+                        motorPower = 0;
+                    }
+                    decrease = false; // won't decrease the motor power if the button is held down
+                } else if (!gamepad1.left_bumper)
+                    decrease = true; // releasing the bumper  will allow you to decrease again
+
+                motorPower = Math.round(motorPower * 100.0) / 100.0; // motor power is sometimes wacky and will have infinite decimals, rounding to fix that
+            }
+        }));
+
+        /*
+         */
+
+
+        /*
+         *    launcherPower
+         */
+
+        // increasing
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            boolean increase = true; //Outside of loop()
+
+            @Override
+            public void loop() {
+                if (gamepad2.right_bumper && increase) {
+                    launcherPower += 0.05;
+                    if (launcherPower > 1) {
+                        launcherPower = 1;
+                    }
+                    increase = false; // won't increase the motor power if the button is held down
+                } else if (!gamepad2.right_bumper)
+                    increase = true; // releasing the bumper  will allow you to increase again
+
+                launcherPower = Math.round(launcherPower * 100.0) / 100.0; // motor power is sometimes wacky and will have infinite decimals, rounding to fix that
+            }
+        }));
+
+        // decreasing
+        registerThread(new TaskThread(new TaskThread.Actions() {
+            boolean decrease = true; //Outside of loop()
+
+            @Override
+            public void loop() {
+                if (gamepad2.left_bumper && decrease) {
+                    launcherPower -= 0.05;
+                    if (launcherPower < 0) {
+                        launcherPower = 0;
+                    }
+                    decrease = false; // won't decrease the motor power if the button is held down
+                } else if (!gamepad2.left_bumper)
+                    decrease = true; // releasing the bumper  will allow you to decrease again
+
+                launcherPower = Math.round(launcherPower * 100.0) / 100.0; // motor power is sometimes wacky and will have infinite decimals, rounding to fix that
+            }
+        }));
+
+        /*
+         */
     }
 
-    // moving motors and checking for strafes
+    /*
+     *    Robot Movement
+     */
     @Override
     public void mainLoop() {
         if (gamepad1.left_stick_x < -0.85) {
@@ -134,8 +237,8 @@ public class TeleOp2020 extends ThreadOpMode {
             backRight.setPower(motorPower * (this.gamepad1.left_stick_y /* +  this.gamepad1.left_stick_x */ - this.gamepad1.right_stick_x));
             backLeft.setPower(motorPower * (-this.gamepad1.left_stick_y /* + this.gamepad1.left_stick_x */ - this.gamepad1.right_stick_x));
         }
-        telemetry.addData("Status", "Running");
         telemetry.addData("Motor Power: ", motorPower);
+        telemetry.addData("Launcher Power: ", launcherPower);
         telemetry.update();
     }
 
